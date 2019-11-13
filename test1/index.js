7e9fe2514ebbd2e370219c14040f1f91bfabb770
sheikoodally <sheikumiii>
2019-11-13 15:15:32 -0500

    change

--- test1/index.js
+++ test1/index.js
@@ -5,47 +5,147 @@ Survey
 
 var json = {
     title: "American History",
-    pages: [
+    "pages": [
         {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "civilwar",
-                    title: "When was the Civil War?",
-                    choices: [
-                        "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                    ],
-                    correctAnswer: "1850-1900"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "libertyordeath",
-                    title: "Who said 'Give me liberty or give me death?'",
-                    choicesOrder: "random",
-                    choices: [
-                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                    ],
-                    correctAnswer: "Patrick Henry"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "magnacarta",
-                    title: "What is the Magna Carta?",
-                    choicesOrder: "random",
-                    choices: [
-                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                    ],
-                    correctAnswer: "The foundation of the British parliamentary system"
-                }
-            ]
+         "name": "page1",
+         "elements": [
+          {
+           "type": "comment",
+           "name": "question1",
+           "title": "Tell me about the employee"
+          },
+          {
+           "type": "radiogroup",
+           "name": "question2",
+           "title": "How well did the employee do?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Excellent"
+            },
+            {
+             "value": "item2",
+             "text": "Good"
+            },
+            {
+             "value": "item3",
+             "text": "Average"
+            },
+            {
+             "value": "item4",
+             "text": "Poor"
+            }
+           ]
+          },
+          {
+           "type": "checkbox",
+           "name": "question3",
+           "title": "How many time he was absent from work in 1 year?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "0-5"
+            },
+            {
+             "value": "item2",
+             "text": "5-10"
+            },
+            {
+             "value": "item3",
+             "text": "10+"
+            }
+           ]
+          },
+          {
+           "type": "radiogroup",
+           "name": "question8",
+           "title": "Does he have strong leadership skills?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Yes"
+            },
+            {
+             "value": "item2",
+             "text": "No"
+            },
+            {
+             "value": "item3",
+             "text": "Average"
+            }
+           ]
+          },
+          {
+           "type": "checkbox",
+           "name": "question5",
+           "title": "How many time he was late for work in a year?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "0-5"
+            },
+            {
+             "value": "item2",
+             "text": "5-10"
+            },
+            {
+             "value": "item3",
+             "text": "10+"
+            }
+           ]
+          },
+          {
+           "type": "radiogroup",
+           "name": "question9",
+           "title": "Is he serious in his work?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Yes"
+            },
+            {
+             "value": "item2",
+             "text": "No"
+            }
+           ]
+          },
+          {
+           "type": "checkbox",
+           "name": "question7",
+           "title": "Rate his reliability",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Excellent"
+            },
+            {
+             "value": "item2",
+             "text": "Good"
+            },
+            {
+             "value": "item3",
+             "text": "Poor"
+            }
+           ]
+          },
+          {
+           "type": "comment",
+           "name": "question10",
+           "title": "Would you recommend him to us and why?"
+          },
+          {
+           "type": "comment",
+           "name": "question4",
+           "title": "What are his strongs? "
+          },
+          {
+           "type": "comment",
+           "name": "question6",
+           "title": "What are his weaknesses?"
+          }
+         ]
         }
-    ],
+       ],
     completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
 
--- test2/single.js
+++ test2/single.js
@@ -5,39 +5,153 @@ Survey
 
 var json = {
     title: "American History",
-    pages: [{
-        questions: [
+    "pages": [
+        {
+         "name": "page1",
+         "elements": [
+          {
+           "type": "comment",
+           "name": "question1",
+           "title": "Tell me about the employee"
+          },
+          {
+           "type": "radiogroup",
+           "name": "question2",
+           "title": "How well did the employee do?",
+           "choices": [
             {
-                type: "radiogroup",
-                name: "civilwar",
-                title: "When was the Civil War?",
-                choices: [
-                    "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                ],
-                correctAnswer: "1850-1900"
+             "value": "item1",
+             "text": "Excellent"
             },
             {
-                type: "radiogroup",
-                name: "libertyordeath",
-                title: "Who said 'Give me liberty or give me death?'",
-                choicesOrder: "random",
-                choices: [
-                    "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                ],
-                correctAnswer: "Patrick Henry"
+             "value": "item2",
+             "text": "Good"
             },
             {
-                type: "radiogroup",
-                name: "magnacarta",
-                title: "What is the Magna Carta?",
-                choicesOrder: "random",
-                choices: [
-                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                ],
-                correctAnswer: "The foundation of the British parliamentary system"
+             "value": "item3",
+             "text": "Average"
+            },
+            {
+             "value": "item4",
+             "text": "Poor"
             }
-        ]
-    }],
+           ]
+          }
+         ]
+        },
+        {
+         "name": "page2",
+         "elements": [
+          {
+           "type": "checkbox",
+           "name": "question5",
+           "title": "How many time he was absent from work in 1 year?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "0-5"
+            },
+            {
+             "value": "item2",
+             "text": "5-10"
+            },
+            {
+             "value": "item3",
+             "text": "10+"
+            }
+           ]
+          },
+          {
+           "type": "radiogroup",
+           "name": "question12",
+           "title": "Does he have strong leadership skills?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Yes"
+            },
+            {
+             "value": "item2",
+             "text": "No"
+            },
+            {
+             "value": "item3",
+             "text": "Average"
+            }
+           ]
+          }
+         ]
+        },
+        {
+         "name": "page3",
+         "elements": [
+          {
+           "type": "checkbox",
+           "name": "question14",
+           "title": "Rate his reliability",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Excellent"
+            },
+            {
+             "value": "item2",
+             "text": "Good"
+            },
+            {
+             "value": "item3",
+             "text": "Poor"
+            }
+           ]
+          },
+          {
+           "type": "radiogroup",
+           "name": "question13",
+           "title": "Is he serious in his work?",
+           "choices": [
+            {
+             "value": "item1",
+             "text": "Yes"
+            },
+            {
+             "value": "item2",
+             "text": "No"
+            }
+           ]
+          }
+         ]
+        },
+        {
+         "name": "page4",
+         "elements": [
+          {
+           "type": "comment",
+           "name": "question7",
+           "title": "Would you recommend him to us and why?"
+          },
+          {
+           "type": "comment",
+           "name": "question9",
+           "title": "What are his strongs? "
+          }
+         ]
+        },
+        {
+         "name": "page5",
+         "elements": [
+          {
+           "type": "comment",
+           "name": "question4",
+           "title": "What are his weaknesses?"
+          },
+          {
+           "type": "comment",
+           "name": "question3",
+           "title": "How would you describe him in short?"
+          }
+         ]
+        }
+       ],
     completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
 
